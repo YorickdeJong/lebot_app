0bb5fe8ae35a75f51338480e9e3cad7d33f1c8f1
Yorick De Jong <yorickdejong@gmail.com>
2023-05-27 12:36:13 +0200

    - changed pool to include connection for heroku to postgresql database

--- server/src/services/postGreSQL.js
+++ server/src/services/postGreSQL.js
@@ -1,32 +1,41 @@
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
 
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'lebot',
-    password: 'Opyorick1',
-    port: 5432,
-    max: 20,
+let pool;
+
+if (process.env.NODE_ENV === 'production') {
+    // In production environment (Heroku), use DATABASE_URL
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    // In development environment (local machine), use local database settings
+    pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'lebot',
+        password: 'Opyorick1',
+        port: 5432,
+        max: 20,
+    });
 }
 
-const pool = new Pool(config);
-
-
 pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
 });
 
 // Test the connection by querying the database
 pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Error executing query', err.stack);
-  } 
-  else {
-    console.log('Successfully connected to PostgreSQL database:', process.env.DB_DATABASE || 'lebot');
-    console.log('Current time in the database:', res.rows[0].now);
-  }
+    if (err) {
+        console.error('Error executing query', err.stack);
+    } 
+    else {
+        console.log('Successfully connected to PostgreSQL database.');
+        console.log('Current time in the database:', res.rows[0].now);
+    }
 });
 
-
-module.exports = pool
+module.exports = pool;
