babb29f90d5990174b230015a4064c4e0c58a4b8
Yorick De Jong <yorickdejong@gmail.com>
2023-05-27 09:00:26 +0200

    - removed package-lock.json

--- server/src/services/postGreSQL.js
+++ server/src/services/postGreSQL.js
@@ -6,10 +6,16 @@ const config = {
     database: 'lebot',
     password: 'Opyorick1',
     port: 5432,
-    max: 5000,
+    max: 20,
 }
 
 const pool = new Pool(config);
+
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 // Test the connection by querying the database
 pool.query('SELECT NOW()', (err, res) => {
@@ -17,9 +23,10 @@ pool.query('SELECT NOW()', (err, res) => {
     console.error('Error executing query', err.stack);
   } 
   else {
-    console.log('Successfully connected to PostgreSQL database:', config.database);
+    console.log('Successfully connected to PostgreSQL database:', process.env.DB_DATABASE || 'lebot');
     console.log('Current time in the database:', res.rows[0].now);
   }
 });
+
 
 module.exports = pool
