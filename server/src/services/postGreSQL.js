6815a1f80ec842305994ccd88889b6662d31dcc4
Yorick De Jong <yorickdejong@gmail.com>
2023-05-28 11:40:10 +0200

    no message

--- server/src/services/postGreSQL.js
+++ server/src/services/postGreSQL.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'production') {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: {
-            rejectUnauthorized: true
+            rejectUnauthorized: false
         }
     });
 } else {
