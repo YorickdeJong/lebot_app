eb31218d8cc57173154f7ed43dff1c73a23f662a
Yorick De Jong <yorickdejong@gmail.com>
2023-05-31 22:41:19 +0200

    no message

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,8 @@ const thinkScreenRouter = require('./routes/think_screen/think_screen.router')
 
 app.use(cors());
 app.use(express.json()); //allows us to get json from end points
-app.use(express.static('public'));
+app.use(express.static('src/public'));
+
 
 
 app.use('/api/v1/assignments', assignmentRoutes);
